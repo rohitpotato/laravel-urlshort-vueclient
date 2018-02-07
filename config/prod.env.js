a5bacbf1caabc40ef643bb2c9aa4974cc7bb1354
= <=>
2018-02-08 00:38:50 +0530

    Second

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_URL: '"https://shielded-cove-84573.herokuapp.com/"'
+  API_URL: '"https://shielded-cove-84573.herokuapp.com/check"'
 }
