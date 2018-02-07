18b8e523709c649c13e8103c577ed8a4b9c4d9f1
= <=>
2018-02-08 03:07:02 +0530

    Commiting

--- dist/server.js
+++ dist/server.js
@@ -23,28 +23,3 @@ app.get('/:code', function (req, res, next) {
 app.get('*', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
-var express = require('express');
-var serveStatic = require('serve-static');
-var axios = require('axios');
-
-app = express();
-app.use(serveStatic(__dirname));
-
-var port = process.env.PORT || 5000;
-var apiUrl = process.env.API_URL || 'http://localhost/urlnode/public';
-
-app.listen(port);
-
-app.get('/:code', function (req, res, next) {
-  axios.get(apiUrl + '?code=' + req.params.code).then(function (response) {
-    res.redirect(301, response.data.data.original_url);
-  }).catch(function (error) {
-    next();
-  });
-}, function (req, res) {
-  res.sendFile(__dirname + '/index.html');
-});
-
-app.get('*', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
-});
